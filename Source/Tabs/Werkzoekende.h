90a7404729e226b24c705da46680db9a526db37d
DPSCryptic <veneman586@gmail.com>
2022-06-28 08:13:28 +0200

    Update Werkzoekende.h

--- Source/Tabs/Werkzoekende.h
+++ Source/Tabs/Werkzoekende.h
@@ -6,7 +6,13 @@ using namespace juce;
 class WerkzoekendeComponent : public Component
 {
 public:
+
+	Werkzoekende();
+
 	void paint(juce::Graphics&) override;
 	void resized() override;
+
+private:
+	juce::TreeView treeView;
 
 };
