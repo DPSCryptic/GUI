191cd30481d3b295caa60326ac9f202a92762d18
DPSCryptic <veneman586@gmail.com>
2022-06-28 12:43:09 +0200

    Update Werkzoekende.h

--- Source/Tabs/Werkzoekende.h
+++ Source/Tabs/Werkzoekende.h
@@ -7,7 +7,7 @@ class ContentComponent : public Component
 {
 public:
 	ContentComponent(Component& contentToDisplay)
-		:  content(contentToDisplay)
+		: content(contentToDisplay)
 	{
 		addAndMakeVisible(content);
 	}
@@ -56,7 +56,6 @@ public:
 		auto deleteButton = treeViewButtonArea.removeFromLeft(treeViewButtonArea.getWidth());
 		auto editViewArea = bounds.removeFromLeft(bounds.getWidth()).reduced(2);
 
-
 		treeView.setBounds(treeViewArea);
 		editArea.setBounds(editViewArea);
 		buttonArea1.setBounds(addButton);
@@ -70,8 +69,8 @@ private:
 	public:
 		TreeViewComponent()
 		{
-			tree.setRootItem(&root);
-			tree.setRootItemVisible(false);
+			//tree.setRootItem(&root);
+			//tree.setRootItemVisible(false);
 
 			addAndMakeVisible(tree);
 		}
@@ -81,59 +80,11 @@ private:
 			tree.setBounds(getLocalBounds());
 		}
 	private:
-		struct RootItem : public TreeViewItem
-		{
-			RootItem()
-			{
-				struct Item : public TreeViewItem
-				{
-					Item(int index, int depth, int numSubItems)
-						: textToDisplay("Item " + String(index)
-							+ ". Depth: " + String(depth)
-							+ ". Num sub-items: " + String(numSubItems))
-					{
-						for (int i = 0; i < numSubItems; ++i)
-							addSubItem(new Item(i,
-								depth + 1,
-								Random::getSystemRandom().nextInt(jmax(0, 5 - depth))));
-					}
 
-					bool mightContainSubItems() override
-					{
-						return getNumSubItems() > 0;
-					}
 
-					void paintItem(Graphics& g, int width, int height) override
-					{
-						if (isSelected())
-						{
-							g.setColour(Colours::yellow.withAlpha(0.3f));
-							g.fillRect(0, 0, width, height);
-						}
 
-						g.setColour(Colours::black);
-						g.drawRect(0, height - 1, width, 1);
+		ListBox tree;
 
-						g.setColour(Colours::white);
-						g.drawText(textToDisplay, 0, 0, width, height, Justification::centredLeft);
-					}
-
-					String getAccessibilityName() override { return textToDisplay; }
-
-					const String textToDisplay;
-				};
-
-				for (int i = 0; i < 10; ++i)
-					addSubItem(new Item(i, 0, Random::getSystemRandom().nextInt(10)));
-			}
-
-			bool mightContainSubItems() override
-			{
-				return true;
-			}
-		};
-		TreeView tree;
-		RootItem root;
 	};
 
 	class EditViewComponent : public Component
@@ -141,70 +92,16 @@ private:
 	public:
 		EditViewComponent()
 		{
-			tree.setRootItem(&root);
-			tree.setRootItemVisible(false);
-
-			addAndMakeVisible(tree);
+			addAndMakeVisible(editor);
 		}
 
 		void resized() override
 		{
-			tree.setBounds(getLocalBounds());
+			editor.setBounds(getLocalBounds());
 		}
 	private:
-		struct RootItem : public TreeViewItem
-		{
-			RootItem()
-			{
-				struct Item : public TreeViewItem
-				{
-					Item(int index, int depth, int numSubItems)
-						: textToDisplay("Item " + String(index)
-							+ ". Depth: " + String(depth)
-							+ ". Num sub-items: " + String(numSubItems))
-					{
-						for (int i = 0; i < numSubItems; ++i)
-							addSubItem(new Item(i,
-								depth + 1,
-								Random::getSystemRandom().nextInt(jmax(0, 5 - depth))));
-					}
 
-					bool mightContainSubItems() override
-					{
-						return getNumSubItems() > 0;
-					}
-
-					void paintItem(Graphics& g, int width, int height) override
-					{
-						if (isSelected())
-						{
-							g.setColour(Colours::yellow.withAlpha(0.3f));
-							g.fillRect(0, 0, width, height);
-						}
-
-						g.setColour(Colours::black);
-						g.drawRect(0, height - 1, width, 1);
-
-						g.setColour(Colours::white);
-						g.drawText(textToDisplay, 0, 0, width, height, Justification::centredLeft);
-					}
-
-					String getAccessibilityName() override { return textToDisplay; }
-
-					const String textToDisplay;
-				};
-
-				for (int i = 0; i < 10; ++i)
-					addSubItem(new Item(i, 0, Random::getSystemRandom().nextInt(10)));
-			}
-
-			bool mightContainSubItems() override
-			{
-				return true;
-			}
-		};
-		TreeView tree;
-		RootItem root;
+		TextEditor editor;
 	};
 
 	class AddButtonViewComponent : public Component
